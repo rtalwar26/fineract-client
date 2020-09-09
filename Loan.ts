3ef7894e0dc8cdf9c584a9451dfbd64fce71f80e
sreelakshmi_yes <sreelakshmiyes@gmail.com>
2020-09-09 16:21:56 +0530

    Modified variable types after  review

--- Loan.ts
+++ Loan.ts
@@ -1,184 +1,182 @@
 import FineractAPI from './FineractAPI';
 
 export interface LoanListConfig {
-  offset?: Number,
-  limit?: Number,
-  orderBy?: String,
-  sortBy?: String,
-  officeId?: Number,
-  underHierarchy?: String,
-  accountNo?: String,
-  externalId?: String,
-  sqlSearch?: String
+  offset?:number,
+  limit?: number,
+  orderBy?: string,
+  sortBy?: string,
+  officeId?: number,
+  underHierarchy?: string,
+  accountNo?: string,
+  externalId?: string,
+  sqlSearch?: string
 }
 
 export interface LoanRetrieveConfig {
 
-  associations?: Array<String>,
-  exclude?: Array<String>,
-  all?: String,
-  repaymentSchedule?: String,
-  futureSchedule?: String,
-  transactions?: String,
-  charges?: String,
-  quarantors?: String,
-  collatoral?: String
+  associations?: Array<string>,
+  exclude?: Array<string>,
+  all?: string,
+  repaymentSchedule?: string,
+  futureSchedule?: string,
+  transactions?: string,
+  charges?: string,
+  quarantors?: string,
+  collatoral?: string
 
 }
 export interface LoanObjectStatusInterface {
 
-  id: Number,
-  code: String,        // value  given in api doc ->loanStatusType.active", Kindly review the variable type
-  value: String,                     // value given in api doc "Active", Kindly review the variable type
-  pendingApproval: Boolean,
-  waitingForDisbursal: Boolean,
-  active: Boolean,
-  closedObligationsMet: Boolean,
-  closedWrittenOff: Boolean,
-  closedRescheduled: Boolean,
-  closed: Boolean,
-  overpaid: Boolean
+  id: number,
+  code: string,        // value  given in api doc ->loanStatusType.active", Kindly review the variable type
+  value: string,                     // value given in api doc "Active", Kindly review the variable type
+  pendingApproval: boolean,
+  waitingForDisbursal: boolean,
+  active: boolean,
+  closedObligationsMet: boolean,
+  closedWrittenOff: boolean,
+  closedRescheduled: boolean,
+  closed: boolean,
+  overpaid: boolean
 
 }
 export interface LoanObjectTypeInterface {
-  id: Number,
-  code: String,                 //"loanType.individual",Kindly review the variable type
-  value: String                //"Individual" ,Kindly review the variable type
+  id: number,
+  code: string,                 //"loanType.individual",Kindly review the variable type
+  value: string                //"Individual" ,Kindly review the variable type
 
 }
 export interface LoanObjectCurrencyInterface {
-  code: String,
-  name: String,
-  decimalPlaces: Number,
-  displaySymbol: String,
-  nameCode: String,                              //"currency.USD",Kindly review the variable type
-  displayLabel: String
+  code: string,
+  name: string,
+  decimalPlaces: number,
+  displaySymbol: string,
+  nameCode: string,                              //"currency.USD",Kindly review the variable type
+  displayLabel: string
 
 }
 export interface LoanObjTermPdFrequencyType {
-  id: Number,
-  code: String,                                 //"termFrequency.periodFrequencyType.months",Kindly review the variable type
-  value: String
+  id: number,
+  code: string,                                 //"termFrequency.periodFrequencyType.months",Kindly review the variable type
+  value: string
 }
 export interface LoanObjRepaymentFrequencyType {
-  id: Number,
-  code: String,    // "repaymentFrequency.periodFrequencyType.months",   Kindly review the variable type
-  value: String   //"Months" Kindly review the variable type
+  id: number,
+  code: string,    // "repaymentFrequency.periodFrequencyType.months",   Kindly review the variable type
+  value: string   //"Months" Kindly review the variable type
 }
 export interface LoanObjInterestRateFrequencyType {
-  id: Number,
-  code: String,                               //"interestRateFrequency.periodFrequencyType.months",Kindly review the type
-  value: String
+  id: number,
+  code: string,                               //"interestRateFrequency.periodFrequencyType.months",Kindly review the type
+  value: string
 }
 export interface LoanObjAmortizationType {
-  id: Number,
-  code: String,                            //"amortizationType.equal.installments", Kindly review the type
-  value: String                               //"Equal installments"
+  id: number,
+  code: string,                            //"amortizationType.equal.installments", Kindly review the type
+  value: string                               //"Equal installments"
 }
 export interface LoanObjInterestType {
-  id: Number,
-  code: String  //"interestType.declining.balance",
-  value: String   //"Declining Balance"
+  id: number,
+  code: string  //"interestType.declining.balance",
+  value: string   //"Declining Balance"
 }
 export interface LoanObjInterestCalculationPeriodType {
-  id: Number,
-  code: String    //"interestCalculationPeriodType.same.as.repayment.period",
-  value: String     // "Same as repayment period"
+  id: number,
+  code: string    //"interestCalculationPeriodType.same.as.repayment.period",
+  value: string     // "Same as repayment period"
 }
 export interface LoanObjTimeline {
-  submittedOnDate: Date,
-  submittedByUsername: String,
-  submittedByFirstname: String,
-  submittedByLastname: String,
-  approvedOnDate: Date,
-  approvedByUsername: String,
-  approvedByFirstname: String,
-  approvedByLastname: String,
-  expectedDisbursementDate: Date,
-  actualDisbursementDate: Date,
-  disbursedByUsername: String,
-  disbursedByFirstname: String,
-  disbursedByLastname: String,
-  expectedMaturityDate: Date
+  submittedOnDate: Array<number>,
+  submittedByUsername: string,
+  submittedByFirstname: string,
+  submittedByLastname: string,
+  approvedOnDate: Array<number>,
+  approvedByUsername: string,
+  approvedByFirstname: string,
+  approvedByLastname: string,
+  expectedDisbursementDate: Array<number>,
+  actualDisbursementDate: Array<number>,
+  disbursedByUsername: string,
+  disbursedByFirstname: string,
+  disbursedByLastname: string,
+  expectedMaturityDate: Array<number>
 }
 export interface LoanObjLinkedAccount {
-  id: Number,
-  accountNo: String
+  id: number,
+  accountNo: string
 }
 export interface LoanObjSummary {
   currency: LoanObjectCurrencyInterface,
-  principalDisbursed:Number,
-  principalPaid:Number,
-  principalWrittenOff:Number,
-  principalOutstanding:Number,
-  principalOverdue:Number,
-  interestCharged:Number,
-  interestPaid:Number,
-  interestWaived:Number,
-  interestWrittenOff:Number,
-  interestOutstanding:Number,
-  interestOverdue:Number,
-  feeChargesCharged:Number,
-  feeChargesDueAtDisbursementCharged:Number,
-  feeChargesPaid:Number,
-  feeChargesWaived:Number,
-  feeChargesWrittenOff:Number,
-  feeChargesOutstanding:Number,
-  feeChargesOverdue:Number,
-  penaltyChargesCharged:Number,
-  penaltyChargesPaid:Number,
-  penaltyChargesWaived:Number,
-  penaltyChargesWrittenOff:Number,
-  penaltyChargesOutstanding:Number,
-  penaltyChargesOverdue:Number,
-  totalExpectedRepayment:Number,
-  totalRepayment:Number,
-  totalExpectedCostOfLoan:Number,
-  totalCostOfLoan:Number,
-  totalWaived:Number,
-  totalWrittenOff:Number,
-  totalOutstanding:Number,
-  totalOverdue:Number,
-  overdueSinceDate:Date
+  principalDisbursed:number,
+  principalPaid:number,
+  principalWrittenOff:number,
+  principalOutstanding:number,
+  principalOverdue:number,
+  interestCharged:number,
+  interestPaid:number,
+  interestWaived:number,
+  interestWrittenOff:number,
+  interestOutstanding:number,
+  interestOverdue:number,
+  feeChargesCharged:number,
+  feeChargesDueAtDisbursementCharged:number,
+  feeChargesPaid:number,
+  feeChargesWaived:number,
+  feeChargesWrittenOff:number,
+  feeChargesOutstanding:number,
+  feeChargesOverdue:number,
+  penaltyChargesCharged:number,
+  penaltyChargesPaid:number,
+  penaltyChargesWaived:number,
+  penaltyChargesWrittenOff:number,
+  penaltyChargesOutstanding:number,
+  penaltyChargesOverdue:number,
+  totalExpectedRepayment:number,
+  totalRepayment:number,
+  totalExpectedCostOfLoan:number,
+  totalCostOfLoan:number,
+  totalWaived:number,
+  totalWrittenOff:number,
+  totalOutstanding:number,
+  totalOverdue:number,
+  overdueSinceDate:Array<number>
 
 }
 
 export interface LoanObjectInterface {
 
-  id: Number,
-  accountNo: Number,
+  id: number,
+  accountNo: number,
   status: LoanObjectStatusInterface,
 
-  clientId: Number,
-  clientName: String,
-  clientOfficeId: Number,
-  loanProductId: Number,
-  loanProductName: String,
-  loanProductDescription: String,
+  clientId: number,
+  clientName: string,
+  clientOfficeId: number,
+  loanProductId: number,
+  loanProductName: string,
+  loanProductDescription: string,
   loanType: LoanObjectTypeInterface,
   currency: LoanObjectCurrencyInterface,
-  principal: Number,
-  termFrequency: Number,
+  principal: number,
+  termFrequency: number,
   termPeriodFrequencyType: LoanObjTermPdFrequencyType,
-  numberOfRepayments: Number,
-  repaymentEvery: Number,
+  numberOfRepayments: number,
+  repaymentEvery: number,
   repaymentFrequencyType: LoanObjRepaymentFrequencyType,
-  interestRatePerPeriod: Number,
+  interestRatePerPeriod: number,
   interestRateFrequencyType: LoanObjInterestRateFrequencyType,
-  annualInterestRate: Number,
+  annualInterestRate: number,
   amortizationType: LoanObjAmortizationType,
   interestType: LoanObjInterestType,
   interestCalculationPeriodType: LoanObjInterestCalculationPeriodType,
-  transactionProcessingStrategyId: Number,
+  transactionProcessingStrategyId: number,
   timeline: LoanObjTimeline,
   linkedAccount: LoanObjLinkedAccount,
   summary:LoanObjSummary,
-  feeChargesAtDisbursementCharged:Number,
-  inArrears:Boolean,
-  isNPA:Boolean
+  feeChargesAtDisbursementCharged:number,
+  inArrears:boolean,
+  isNPA:boolean
 }
-
-
 export default class Loan {
 
   fineract_obj: FineractAPI
